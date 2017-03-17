ec77a9bef86ac4495913a4b26083cf0bc670e7ce
Dan Skelly <daniel.skelly@jax.org>
2017-03-17 11:32:23 -0400

    move fasta reading code to a separate module

--- tile_across_genome.py
+++ tile_across_genome.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 """Write a BED file of windows across the genome"""
-import sys, argparse, re
+import sys, argparse, re, seqs
 
 def main():
     parser = argparse.ArgumentParser()
@@ -21,7 +21,7 @@ def main():
         "B in each chromosome name")
     args = parser.parse_args()
 
-    for faObj in readFastaByChrom(args.genome):
+    for faObj in seqs.readFastaByChrom(args.genome):
         l = len(faObj.seq)
         n_full_bins = l/args.windowSize
         # center the full bins along the chrom
@@ -49,48 +49,6 @@ def main():
             else:
                 print "{}\t{}\t{}".format(faObj.name, start, end)
 
-class Fasta:
-    def __init__(self, name, seq=None):
-        self.name = name
-        self.seq = seq
-    def __repr__(self):
-        return ">%s\n%s".format(self.name, self.seq)
-
-def readFastaByChrom(filename):
-    """Read a file in fasta format. Return an iterator of Fasta objects where
-    each object is a fasta sequence in the file. Uses a generator for
-    memory-efficient reading in of the sequences"""
-    f = open(filename)
-    file_pos = f.tell()
-    line = f.readline()
-    while line.startswith('#'):
-        file_pos = f.tell()
-        line = f.readline()
-    f.seek(file_pos)
-    while True:
-        yield parseFastaMatch(f)
-    f.close()
-
-def parseFastaMatch(filehandle):
-    "Read in lines representing a single fasta sequence"
-    readName = filehandle.readline().rstrip('\n')[1:]
-    if readName == '':
-        raise StopIteration
-    fastaObj = Fasta(name=readName)
-    filePos = filehandle.tell()
-    line = filehandle.readline().rstrip('\n')
-    seq = [line]
-    while True:
-        filePos = filehandle.tell()
-        line = filehandle.readline().rstrip('\n')
-        if line == '': break
-        elif line.startswith('>'):
-            filehandle.seek(filePos)
-            break
-        else:
-            seq.append(line)
-    fastaObj.seq = ''.join(seq)
-    return fastaObj
 
 if __name__ == "__main__":
     sys.exit(main())
